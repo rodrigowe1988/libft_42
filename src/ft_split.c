e0372731baabe896d924a96201c48ac845631241
Rodrigo Weber <rodrigoweber97@hotmail.com>
2021-09-07 10:56:15 -0300

    sending_ft_split.c

--- src/ft_split.c
+++ src/ft_split.c
@@ -20,3 +20,67 @@ static size_t	ft_countdown(const char *s, char c)
 	}
 	return (words);
 }
+
+static size_t	get_wordlen(const char *s, char c)
+{
+	size_t	offset;
+
+	offset = 0;
+	while (s[offset] && s[offset] != c)
+		offset++;
+	return (offset++);
+}
+
+static char		*worddup(const char *s, size_t len)
+{
+	char	*str;
+	size_t	offset;
+
+	str = malloc(len + 1);
+	if (str == NULL)
+		return (NULL);
+	offset = 0;
+	while (offset < len)
+	{
+		str[offset] = s[offset];
+		offset++;
+	}
+	str[offset] = '\0';
+	return (str);
+}
+
+static void 	*freesplit(char	**tab, size_t stop)
+{
+	size_t	counter;
+
+	counter = 0;
+	while (counter < stop)
+		free(tab[counter]);
+	free(tab);
+	return (NULL);
+}
+
+char	**ft_split(const char *s, char c)
+{
+	char	**tab;
+	size_t	len;
+	size_t	words;
+	size_t	counter;
+
+	if (s == NULL)
+		return (NULL);
+	words = ft_countdown(s, c);
+	tab = malloc((words + 1) * sizeof(char*));
+	if (tab ==  NULL)
+		return (NULL);
+	counter = 0;
+	while (counter < words)
+	{
+		len = get_wordlen(s, c);
+		if (len)
+		{
+			tab[counter] =  worddup(s, len);
+			if (tab[counter++] == NULL)
+		}
+	}
+}
