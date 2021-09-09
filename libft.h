a7e35b2a7d48363f95e6a91f65f1a9b5375da2a8
Rodrigo Weber <rodrigoweber97@hotmail.com>
2021-09-09 08:03:19 -0300

    changing_libft.h

--- libft.h
+++ libft.h
@@ -4,33 +4,34 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int     ft_atoi(char *str);
-void    ft_bzero(void *str, size_t n);
+int		ft_atoi(char *str);
+void	ft_bzero(void *str, size_t n);
 void	*ft_calloc(size_t count, size_t size);
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
 char	*ft_itoa(int n);
-int	    ft_isalnum(int c);
-int	    ft_isalpha(int c);
-int	    ft_isascii(int c);
-int	    ft_isdigit(int c);
-int	    ft_isprint(int c);
-int     ft_strlen(char c[]);
-void  	*ft_memchr(const void *s, int c, size_t n);
-int  	memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
-void    *memmove(void *dest, const void *src, size_t n);
+int		memcmp(const void *s1, const void *s2, size_t n);
+void	*memmove(void *dest, const void *src, size_t n);
 void	*ft_memset(void *ptr, int c, size_t n);
 char	**ft_split(const char *s, char c);
+char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char	*src);
 char	*ft_strjoin(char const *s1, char const *s2);
-char    *ft_strmapi(char const *s, char (*f)(unsigned int, char));
-char    *ft_substr(char const *s, unsigned int start, size_t len);
-char  	*ft_strchr(const char *s, int c);
 size_t	ft_strlcat(char *dest, char *src, size_t size);
 size_t	ft_strlcpy(char *dest, const char *src, size_t destsize);
-int 	ft_strlen(char c[]);
+int		ft_strlen(char c[]);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
 char	*ft_strrchr(const char *s, int c);
-int     ft_toupper(int c);
-int     ft_tolower(int c);
+char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
 
 #endif
